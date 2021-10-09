c63d591d022c7508c0120c183ba81ad512beb717
Zhang Maiyun <myzhang1029@hotmail.com>
2021-10-09 16:27:23 -0700

    Implement Windows getip
    Signed-off-by: Zhang Maiyun <myzhang1029@hotmail.com>

--- src/getip/libc_getips.rs
+++ src/getip/libc_getips.rs
@@ -5,25 +5,28 @@ use std::ffi::CStr;
 use std::mem;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::ptr;
+#[cfg(unix)]
 use std::str::FromStr;
 #[cfg(windows)]
 use winapi::{
     shared::{
-        ntdef::{DWORD, ULONG},
+        minwindef::{DWORD, LPVOID, ULONG},
         winerror, ws2def,
-        ws2def::{SOCKADDR_IN, SOCKADDR_IN6},
+        ws2def::{SOCKADDR, SOCKADDR_IN},
+        ws2ipdef::SOCKADDR_IN6,
     },
     um::{
         heapapi::{GetProcessHeap, HeapAlloc, HeapFree},
         iphlpapi::GetAdaptersAddresses,
         iptypes::{
-            GAA_FLAG_SKIP_DNS_SERVER, GAA_FLAG_SKIP_FRIENDLY_NAME, GAA_FLAG_SKIP_MULTICAST,
-            IP_ADAPTER_ADDRESSES, IP_ADAPTER_ANYCAST_ADDRESS, IP_ADAPTER_UNICAST_ADDRESS,
+            GAA_FLAG_INCLUDE_PREFIX, GAA_FLAG_SKIP_DNS_SERVER, GAA_FLAG_SKIP_FRIENDLY_NAME,
+            GAA_FLAG_SKIP_MULTICAST, IP_ADAPTER_ADDRESSES, IP_ADAPTER_ANYCAST_ADDRESS,
+            IP_ADAPTER_UNICAST_ADDRESS,
         },
     },
 };
 #[cfg(windows)]
-const INITIAL_ALLOC_SIZE: usize = 15000;
+const INITIAL_ALLOC_SIZE: ULONG = 15000;
 #[cfg(windows)]
 const MAX_TRIES: usize = 5;
 
@@ -38,6 +41,7 @@ macro_rules! fail_os_err {
 #[cfg(unix)]
 unsafe fn get_addr_for_ifa_unix(addr: libc::ifaddrs, ip_type: Option<IpType>) -> Result<IpAddr> {
     let sockaddr = addr.ifa_addr;
+    assert!(!sockaddr.is_null());
     let family = libc::c_int::from((*sockaddr).sa_family);
     // Has a IP family filter
     if let Some(ip_type) = ip_type {
@@ -127,36 +131,38 @@ pub fn get_iface_addrs(ip_type: Option<IpType>, iface_name: Option<&str>) -> Res
     }
 }
 
-/// Extract all addresses from an adapter
+/// Convert pointer to a `SOCKADDR` to Rust IpAddr
+/// `raw_addr` must not be NULL
 #[cfg(windows)]
-fn extract_addresses(adapter: &IP_ADAPTER_ADDRESSES) -> Vec<IpAddr> {
-    let addresses: Vec<IpAddr> = Vec::new();
-    let mut cur_unicast: *mut IP_ADAPTER_UNICAST_ADDRESS = adapter.FirstUnicastAddress;
-    while (!cur_unicast.is_null()) {
+unsafe fn sockaddr_to_ipaddr(raw_addr: *mut SOCKADDR) -> IpAddr {
+    if (*raw_addr).sa_family as i32 == ws2def::AF_INET {
+        let saddr_in = raw_addr as *mut SOCKADDR_IN;
+        let saddr_in_addr = (*saddr_in).sin_addr.S_un.S_addr();
+        IpAddr::V4(Ipv4Addr::from(*saddr_in_addr))
+    } else {
+        let saddr_in = raw_addr as *mut SOCKADDR_IN6;
+        let saddr_in_addr = (*saddr_in).sin6_addr.u.Byte();
+        IpAddr::V6(Ipv6Addr::from(*saddr_in_addr))
+    }
+}
+
+/// Extract all addresses from an adapter
+/// `adapter` must not be NULL
+#[cfg(windows)]
+unsafe fn extract_addresses(adapter: *mut IP_ADAPTER_ADDRESSES) -> Vec<IpAddr> {
+    let mut addresses: Vec<IpAddr> = Vec::new();
+    let mut cur_unicast: *mut IP_ADAPTER_UNICAST_ADDRESS = (*adapter).FirstUnicastAddress;
+    while !cur_unicast.is_null() {
         let raw_addr = (*cur_unicast).Address.lpSockaddr;
-        if (*raw_addr).sa_family == ws2def.AF_INET {
-            let saddr_in = raw_addr as *mut SOCKADDR_IN;
-            let saddr_in_addr = saddr_in.sin_addr.S_un.S_un_b();
-            addresses.push(IpAddr::V4(Ipv4Addr::from(saddr_in_addr)));
-        } else {
-            let saddr_in = raw_addr as *mut SOCKADDR_IN6;
-            let saddr_in_addr = saddr_in.sin6_addr.u.Byte();
-            addresses.push(IpAddr::V6(Ipv6Addr::from(saddr_in_addr)));
-        }
+        assert!(!raw_addr.is_null());
+        addresses.push(sockaddr_to_ipaddr(raw_addr));
         cur_unicast = (*cur_unicast).Next;
     }
-    let mut cur_anycast: *mut IP_ADAPTER_ANYCAST_ADDRESS = adapter.FirstAnycastAddress;
-    while (!cur_anycast.is_null()) {
+    let mut cur_anycast: *mut IP_ADAPTER_ANYCAST_ADDRESS = (*adapter).FirstAnycastAddress;
+    while !cur_anycast.is_null() {
         let raw_addr = (*cur_anycast).Address.lpSockaddr;
-        if (*raw_addr).sa_family == ws2def.AF_INET {
-            let saddr_in = raw_addr as *mut SOCKADDR_IN;
-            let saddr_in_addr = saddr_in.sin_addr.S_un.S_un_b();
-            addresses.push(IpAddr::V4(Ipv4Addr::from(saddr_in_addr)));
-        } else {
-            let saddr_in = raw_addr as *mut SOCKADDR_IN6;
-            let saddr_in_addr = saddr_in.sin6_addr.u.Byte();
-            addresses.push(IpAddr::V6(Ipv6Addr::from(saddr_in_addr)));
-        }
+        assert!(!raw_addr.is_null());
+        addresses.push(sockaddr_to_ipaddr(raw_addr));
         cur_anycast = (*cur_anycast).Next;
     }
     addresses
@@ -169,24 +175,26 @@ fn extract_addresses(adapter: &IP_ADAPTER_ADDRESSES) -> Vec<IpAddr> {
 /// https://docs.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getadaptersaddresses
 #[cfg(windows)]
 pub fn get_iface_addrs(ip_type: Option<IpType>, iface_name: Option<&str>) -> Result<Vec<IpAddr>> {
-    let family = match ip_type {
+    let family: u32 = match ip_type {
         Some(IpType::Ipv4) => ws2def::AF_INET,
         Some(IpType::Ipv6) => ws2def::AF_INET6,
         None => ws2def::AF_UNSPEC,
-    };
+    } as u32;
     let flags: ULONG = GAA_FLAG_INCLUDE_PREFIX
         | GAA_FLAG_SKIP_DNS_SERVER
         | GAA_FLAG_SKIP_MULTICAST
         | GAA_FLAG_SKIP_FRIENDLY_NAME;
     // Allocate a 15 KB buffer to start with.
     let mut allocated_size: ULONG = INITIAL_ALLOC_SIZE;
-    let adapter_addresses: PIP_ADAPTER_ADDRESSES;
-    let return_value: DWORD = 0;
+    let mut adapter_addresses: *mut IP_ADAPTER_ADDRESSES;
+    let mut return_value: DWORD = 0;
     unsafe {
+        // Silence maybe uninitialized error
+        adapter_addresses = mem::zeroed();
         // Try several times to query the resources as suggested by doc
         for _ in 0..MAX_TRIES {
-            adapter_addresses =
-                HeapAlloc(GetProcessHeap(), 0, allocated_size) as PIP_ADAPTER_ADDRESSES;
+            adapter_addresses = HeapAlloc(GetProcessHeap(), 0, allocated_size as usize)
+                as *mut IP_ADAPTER_ADDRESSES;
             if adapter_addresses.is_null() {
                 return fail_os_err!();
             }
@@ -197,35 +205,43 @@ pub fn get_iface_addrs(ip_type: Option<IpType>, iface_name: Option<&str>) -> Res
                 adapter_addresses,
                 &mut allocated_size,
             );
-            if return_value == ERROR_BUFFER_OVERFLOW {
-                HeapFree(GetProcessHeap(), 0, adapter_addresses);
+            if return_value == winerror::ERROR_BUFFER_OVERFLOW {
+                HeapFree(GetProcessHeap(), 0, adapter_addresses as LPVOID);
             } else {
                 break;
             }
         }
     }
     let result = if return_value == winerror::NO_ERROR {
-        let addresses: Vec<IpAddr> = Vec::new();
+        let mut addresses: Vec<IpAddr> = Vec::new();
         let mut curr_adapter = adapter_addresses;
         while !curr_adapter.is_null() {
-            let adapter_name = (*curr_adapter).AdapterName as *const libc::c_char;
-            let adapter_name = CStr::from_ptr(adapter_name).to_bytes();
-            let adapter_name = std::str::from_utf8_unchecked(adapter_name);
-            if let Some(expected_adapter_name) = iface_name {
-                if adapter_name == expected_adapter_name {
-                    addresses.append(extract_addresses(curr_adapter));
+            unsafe {
+                let adapter_name = (*curr_adapter).AdapterName as *const libc::c_char;
+                let adapter_name = CStr::from_ptr(adapter_name).to_bytes();
+                let adapter_name = std::str::from_utf8_unchecked(adapter_name);
+                if let Some(expected_adapter_name) = iface_name {
+                    if adapter_name == expected_adapter_name {
+                        let mut addrs = extract_addresses(curr_adapter);
+                        addresses.append(&mut addrs);
+                    }
+                } else {
+                    let mut addrs = extract_addresses(curr_adapter);
+                    addresses.append(&mut addrs);
                 }
-            } else {
-                addresses.append(extract_addresses(curr_adapter));
+                curr_adapter = (*curr_adapter).Next;
             }
-            curr_adapter = (*curr_adapter).Next;
         }
         Ok(addresses)
     } else {
         // Let Rust interpret the error for me
-        Err(std::io::Error::from_raw_os_error(return_value));
+        Err(Error::IoError(std::io::Error::from_raw_os_error(
+            return_value as i32,
+        )))
     };
-    HeapFree(GetProcessHeap(), 0, adapter_addresses);
+    unsafe {
+        HeapFree(GetProcessHeap(), 0, adapter_addresses as LPVOID);
+    }
     result
 }
 
